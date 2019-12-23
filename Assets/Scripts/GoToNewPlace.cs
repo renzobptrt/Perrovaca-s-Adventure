af75b046839d127ec7ddc07326f9a8c5203f3aa3
renzobptrt <renzobptrt@gmail.com>
2019-12-22 21:43:05 -0500

    Mejoramiento de RightTopMainScene

---
+++ Assets/Scripts/GoToNewPlace.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToNewPlace : MonoBehaviour
+{
+    public string newPlace = "New Place Scene";
+    public string goToPlaceName;
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag.Equals("Player")){
+            FindObjectOfType<PlayerController>().nextPlaceName = goToPlaceName;
+            SceneManager.LoadScene(newPlace);
+        }
+    }
+
+}
