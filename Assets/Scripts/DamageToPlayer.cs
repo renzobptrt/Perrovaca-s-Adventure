6e652cb1a31fc3dfa4c499af6d8c195c3a3d2c93
renzobptrt <renzobptrt@gmail.com>
2019-12-26 01:11:03 -0500

    Enemy, Enemy movement, Damage Enemy - Player,Player Weapon

---
+++ Assets/Scripts/DamageToPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageToPlayer : MonoBehaviour
+{   
+
+    /*public float timeToRevivePlayer;
+    private float timeRivalvalCounter;
+    private bool playerReviving;
+
+    private GameObject thePlayer;*/
+
+    public int damage;
+
+    // Update is called once per frame
+    /*void Update()
+    {
+        if(playerReviving){
+            timeRivalvalCounter -= Time.deltaTime;
+            if(timeRivalvalCounter < 0 ){
+                playerReviving = false;
+                thePlayer.SetActive(true);
+            }
+        }
+    }*/
+
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.tag.Equals("Player")){
+            //Colision entre enemigo y jugador
+            /*collision.gameObject.SetActive(false);
+            playerReviving = true;
+            timeRivalvalCounter = timeToRevivePlayer;
+            thePlayer = collision.gameObject;*/
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(damage);  
+        }
+    }
+}
---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{   
+    /*
+        Script que gobernará el movimiento del enemigo
+    */
+    
+    //Caracteristicas
+    public float enemySpeed = 1;
+    private bool isMoving;
+
+    //Componentes
+    private Rigidbody2D enemyRgb;
+    private Animator enemyAnimator;
+    private const string horizontal = "Horizontal";
+    private const string vertical = "Vertical";
+    
+    //Combinaciones de movimiento
+    public float timeBetweenSteps; //Tiempo entre movimiento
+    private float timeBetweenStepsCounter; //Cuanto tiempo paso desde el ultimo movimiento
+    public float timeToMakeStep; //Tiempo que tarde hacer el paso
+    private float timeToMakeStepCounter; //Tiempo desde que inicio el movimiento
+    public Vector2 directionToMakeStep;
+
+    void Awake(){
+        this.enemyRgb = GetComponent<Rigidbody2D>();
+        this.enemyAnimator = GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        timeBetweenStepsCounter = timeBetweenSteps*Random.Range(0.5f,1.5f);
+        timeToMakeStepCounter = timeToMakeStep*Random.Range(0.5f,1.5f);
+    }
+
+
+    void Update()
+    {
+        if(isMoving){
+            timeToMakeStepCounter -= Time.deltaTime;
+            enemyRgb.velocity = directionToMakeStep;
+
+            if(timeToMakeStepCounter<0){
+                isMoving = false;
+                timeBetweenStepsCounter = timeBetweenSteps;
+                enemyRgb.velocity = Vector2.zero;   
+            }
+        }
+        else
+        {
+            timeBetweenStepsCounter -= Time.deltaTime;
+
+            if(timeBetweenStepsCounter<0){
+                isMoving = true;
+                timeToMakeStepCounter = timeToMakeStep;
+                directionToMakeStep = new Vector2(Random.Range(-1,2),
+                Random.Range(-1,2))*enemySpeed;
+            }    
+        }
+        
+
+        enemyAnimator.SetFloat(horizontal,directionToMakeStep.x);
+        enemyAnimator.SetFloat(vertical, directionToMakeStep.y);
+    }
+}
