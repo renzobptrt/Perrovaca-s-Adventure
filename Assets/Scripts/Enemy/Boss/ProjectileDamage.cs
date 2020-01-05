3be5c4021a63af5005ea539b60800d888aa20669
renzobptrt <renzobptrt@gmail.com>
2020-01-05 17:56:53 -0500

    Projectile Damage Script

---
+++ Assets/Scripts/Enemy/Boss/ProjectileDamage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileDamage : MonoBehaviour
+{
+    public int damage;
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag.Equals("Player")){
+            //For less damage with player defense
+            CharacterStats stast = collision.gameObject.GetComponent<CharacterStats>();
+            int totalDamage = damage - stast.defenseLevels[stast.currentLevel];
+            if(totalDamage < 0){
+                totalDamage = 1;
+            }
+            collision.gameObject.GetComponent<HealthManager>().DamageCharacter(totalDamage);  
+        }
+    }
+}
